c83dd93c4bf06cae18181ddc7267d95d101af1cf
Brian Shin <bys8@cornell.edu>
2014-06-25 12:02:06 -0400

    Created a new project called 'inelastic'. For now, just a clone of 'eplus'.

---
+++ inelastic/src/PositronPhysicsList1.cc
@@ -0,0 +1,136 @@
+//
+/*
+ Physics list for positron-electron annihilation events
+ Background specific
+ Dark Photon group
+ 
+ !!!History
+    CJC 6.11.14 Created
+    cJC 6.18.14 Modified likelihood of collision
+
+ */
+
+#include "PositronPhysicsList1.hh"
+
+
+#include "G4ProcessManager.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4Positron.hh"
+#include "G4Electron.hh"
+#include "G4Proton.hh"
+#include "G4Gamma.hh"
+
+#include "G4VUserPhysicsList.hh"
+
+#include "G4hIonisation.hh"
+#include "G4eIonisation.hh"
+
+#include "G4hBremsstrahlung.hh"
+#include "G4eBremsstrahlung.hh"
+
+#include "G4eplusAnnihilation.hh"
+
+// ####################################### BRIAN SHIN ####################################
+#include "G4PositronNuclearProcess.hh"
+#include "G4ElectroVDNuclearModel.hh"
+
+#include "G4BosonConstructor.hh"
+#include "G4LeptonConstructor.hh"
+#include "G4MesonConstructor.hh"
+#include "G4BaryonConstructor.hh"
+#include "G4IonConstructor.hh"
+#include "G4ShortLivedConstructor.hh"
+// ####################################### BRIAN SHIN ####################################
+
+#include "G4VEmProcess.hh"
+
+
+PositronPhysicsList1::PositronPhysicsList1()
+{
+  defaultCutValue = 1.0*mm;
+  SetVerboseLevel(0);
+}
+
+PositronPhysicsList1::~PositronPhysicsList1()
+{}
+
+void PositronPhysicsList1::ConstructParticle()
+{
+  //G4Gamma::GammaDefinition();
+  //G4Positron::PositronDefinition();
+  //G4Electron::ElectronDefinition();
+  //G4Proton::ProtonDefinition();
+
+  G4BosonConstructor bosonCons;
+  bosonCons.ConstructParticle();
+
+  G4LeptonConstructor lepCons;
+  lepCons.ConstructParticle();
+
+  G4MesonConstructor mesonCons;
+  mesonCons.ConstructParticle();
+
+  G4IonConstructor ionCons;
+  ionCons.ConstructParticle();
+
+  G4BaryonConstructor baryonCons;
+  baryonCons.ConstructParticle();
+
+  G4ShortLivedConstructor slCons;
+  slCons.ConstructParticle();
+}
+
+void PositronPhysicsList1::ConstructProcess()
+{
+  AddTransportation();
+  //ConstructEM(); // commented out to isolate positron-nuclear process
+  ConstructHadronic();
+
+}
+
+void PositronPhysicsList1::ConstructEM()
+{
+  G4ParticleDefinition* positron = G4Positron::PositronDefinition();
+  G4ProcessManager* pman = positron->GetProcessManager();
+  
+
+
+  G4VEmProcess* eplusProc = new G4eplusAnnihilation();
+  eplusProc->SetCrossSectionBiasingFactor(1e+06, true);
+  pman->AddProcess(eplusProc, 0, -1, 4);
+  
+
+  //pman->AddProcess(new G4eplusAnnihilation(), 0, -1, 4);
+ 
+
+}
+
+// ####################################### BRIAN SHIN ####################################
+void PositronPhysicsList1::ConstructHadronic()
+{
+  // get pointer to positron process manager
+  G4ParticleDefinition* positron = G4Positron::PositronDefinition();
+  G4ProcessManager* pman = positron->GetProcessManager();
+
+  // get pointer to positron-nuclear process
+  G4PositronNuclearProcess* pnproc = new G4PositronNuclearProcess("PositronNuclear");
+  
+  // register positron-nuclear model
+  G4ElectroVDNuclearModel* pnmodel = new G4ElectroVDNuclearModel();
+  pnproc->RegisterMe(pnmodel);
+
+  // set biasing
+  pnproc->BiasCrossSectionByFactor(1000.);
+
+  // add process to process manager
+  pman->AddDiscreteProcess(pnproc);
+}
+// ###################################### BRIAN SHIN ####################################
+
+void PositronPhysicsList1::SetCuts()
+{
+  SetCutValue(defaultCutValue, "gamma");
+  SetCutValue(defaultCutValue, "e-");
+  SetCutValue(defaultCutValue, "e+");
+  SetCutValue(defaultCutValue, "proton");
+}
---
+++ inelastic/src/RunAction.cc
@@ -0,0 +1,98 @@
+/*
+ * Dark Photon Run Action
+ * !!!History 
+ *    CJC 6.14.15 created
+ *
+ * file RunAction.cc
+ * Description: 
+ * Run Action is called for every run. Consider what data should be collected
+ * over long runs
+ */
+
+
+
+#include "RunAction.hh"
+
+#include "G4Run.hh"
+#include "G4RunManager.hh"
+
+#include "Analysis.hh"
+#include "G4SystemOfUnits.hh"
+
+
+//!!!
+//Initialize analysis storing i.e. ntuples or histograms
+RunAction::RunAction()
+  : G4UserRunAction()
+{
+  //set printing event number for each event
+  G4RunManager::GetRunManager()->SetPrintProgress(1);
+
+  // Data storage and analysis
+  G4AnalysisManager* analysisMan = G4AnalysisManager::Instance();
+  G4cout << "Using" << analysisMan->GetType() << G4endl;
+  analysisMan->SetVerboseLevel(2);
+
+  //Creating directories
+   analysisMan->SetNtupleDirectoryName("ntuples");
+
+   analysisMan->CreateH1("Photon_Hits", 
+			 "Photon Hits", 
+			 10, 
+			 -.5,
+			 3., 
+			 "none",
+			 "none",
+			 "linear");
+
+
+   analysisMan->CreateNtuple("Gamma_Info", "Photon hits");
+   analysisMan->CreateNtupleIColumn("numHits"); // 0
+   analysisMan->CreateNtupleDColumn("photon1xPos"); // 1
+   analysisMan->CreateNtupleDColumn("photo1yPos"); // 2
+   analysisMan->CreateNtupleDColumn("totEnergyPhoton1"); // 3
+   analysisMan->CreateNtupleDColumn("photon2xPos"); //4
+   analysisMan->CreateNtupleDColumn("photon2yPos"); //5
+   analysisMan->CreateNtupleDColumn("totEnergyPhoton2"); // 6
+   analysisMan->CreateNtupleDColumn("theta1"); //7
+   analysisMan->CreateNtupleDColumn("theta2"); //8
+   analysisMan->CreateNtupleIColumn("crystalNum1"); //9
+   analysisMan->CreateNtupleIColumn("cyrstalNum2"); //10
+   analysisMan->CreateNtupleIColumn("ringNum1"); // 11
+   analysisMan->CreateNtupleIColumn("ringNum2"); // 12 
+   analysisMan->FinishNtuple();
+  
+
+}
+
+
+//delete your analysis manager
+RunAction::~RunAction()
+{
+  delete G4AnalysisManager::Instance();
+}
+
+//!!!
+//Data storage
+void RunAction::BeginOfRunAction(const G4Run*)
+{
+
+  //Save random number seed
+  // G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+  
+  // Data storage and analysis
+  G4AnalysisManager* analysisMan = G4AnalysisManager::Instance();
+ 
+   analysisMan->OpenFile("TwoGamma");
+
+}
+
+//!!!
+//Finish and close data file
+void RunAction::EndOfRunAction(const G4Run* )
+{
+  
+  G4AnalysisManager* analysisMan = G4AnalysisManager::Instance();
+  analysisMan->Write();
+  analysisMan->CloseFile();
+}
