653d0dc5a0310780534b703a8b00dd83ceab79ac
local1 <local1@localhost.localdomain>
2014-07-03 12:32:33 -0400

    added some more stuff
    Former-commit-id: 27bf2a1b99d4bd5897f9f80dda76fe9b49980589

---
+++ testbuild/buildme.sh
@@ -0,0 +1,1 @@
+cmake -DGEANT4_BUILD_MULTITHREADED=ON -DGeant4_DIR=/usr/local/geant4.10.00.p01/lib64/Geant4-10.0.0 -DHEPMC_INCLUDE_DIR=../hepmc -DHEPMC_LIBRARIES=../hepmcbuild/lib/libHepMC.so ../HepMCEx02
