bdd742da9c5c72f9970c16073941acc3bb95b919
local1 <local1@localhost.localdomain>
2014-07-03 12:31:20 -0400

    Cleaned up the git a bit in prep for tests.

--- hepmcbuild/examples/pythia8/config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ ! $?LD_LIBRARY_PATH ]; then
-  export LD_LIBRARY_PATH=/usr/local/lib
-fi
-if [ $?LD_LIBRARY_PATH ]; then
-  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
-fi
-export PYTHIA8DATA=${PYTHIA8_HOME}/xmldoc
--- hepmcbuild/test/CMakeFiles/testFlow.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for testFlow.
-# This may be replaced when dependencies are built.
