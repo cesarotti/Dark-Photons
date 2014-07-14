c6a38df462f967b19ade5e6957d11e777b5df80b
eyvindn <eyvind.niklasson@gmail.com>
2014-07-14 17:09:22 -0400

    Latest madgraph runs added.
    Former-commit-id: 74e8de7c78c4808e6a6d8d0645395af1da85f87b

--- 4PlotsMG/plots.C
+++ 4PlotsMG/plots.C
@@ -6,62 +6,144 @@
 #include <iostream>
 #include "TCanvas.h"
 
-const double POSITRON_ENERGY = 5000.; //MeV
-const double ELECTRON_MASS = .511; //MeV/c^2
-const double GAMMA_CM_2 = POSITRON_ENERGY/(2*ELECTRON_MASS);
-
-double mSquared(double energy, double theta)
-{
-  double thetaRad = theta*TMath::Pi()/180.; 
-  return (2*ELECTRON_MASS*(POSITRON_ENERGY-energy*(1+GAMMA_CM_2*pow(thetaRad, 2.))));
-}
-
 void plots() {
 
-  double yPos, xPos, theta, energyTot = 0.;
-  int numHit, id = 0;
+  double yPos, xPos, theta, energy = 0.;
+  int numHit, pID = 0;
 
   TFile* file = new TFile("complete.root");
 
-  TTree* tree = (TTree *)file->Get("Signal");
+  TTree* Hits_Info = (TTree *)file->Get("Signal");
 
-  int nEvents = tree->GetEntries(); 
+  Hits_Info->SetBranchAddress("numHits", &numHit);
+  Hits_Info->SetBranchAddress("energyTot", &energy);
+  Hits_Info->SetBranchAddress("XPosition", &xPos);
+  Hits_Info->SetBranchAddress("YPosition", &yPos);
+  Hits_Info->SetBranchAddress("Particle_ID", &pID);
+  Hits_Info->SetBranchAddress("Theta", &theta);
 
-  tree->SetBranchAddress("numHits", &numHit);
-  tree->SetBranchAddress("energyTot", &energyTot);
-  tree->SetBranchAddress("XPosition", &xPos);
-  tree->SetBranchAddress("YPosition", &yPos);
-  tree->SetBranchAddress("Particle_ID", &id);
-  tree->SetBranchAddress("Theta", &theta);
 
-  TH2D* engTheta = new TH2D("engTheta", "energy_v_theta", 50, 2, 5, 50, 10, 510);
 
-  TH1D* energyH = new TH1D("energyH", "Energy", 100, 10, 510);
+  // Numbers
+  // check to make sure values correspond to given project
+  // (i.e. bias factor is right, etc)
+  int neplus = 10000000;
+  double biasFactor = 10000;
+  int eplusPerSec = 6000000000;
+  double scaleFactor = eplusPerSec/(neplus*biasFactor);
 
-  TH1D* thetaH = new TH1D("thetaH", "Theta", 50, 2, 5);
+  double pi = 3.14159265359;
+  double me = 0.510998928; // MeV
 
-  TH1D* M2 = new TH1D("M2", "M^2", 100, -5000, 5000);
 
-  for (int i=0; i<nEvents; i++)
-    {
-      tree->GetEntry(i);
-      engTheta->Fill(theta, energyTot ); 
-      energyH->Fill(energyTot);
-      thetaH->Fill(theta);
-      M2->Fill(mSquared(energyTot, theta));
+
+
+  // Initialize histograms
+  double nEnergyMin = 10; // MeV
+  double nEnergyMax = 510; // MeV
+  int nEnergyBins = 50;
+  double dEnergyBinSize = (double)(nEnergyMax - nEnergyMin) / (double)nEnergyBins;
+
+  double nThetaMin = 34; // mrad
+  double nThetaMax = 88; // mrad
+  int nThetaBins = 50;
+  double dThetaBinSize = (nThetaMax - nThetaMin) / nThetaBins;
+
+  double nM2Min = -5000; // MeV
+  double nM2Max = 5000; // MeV
+  int nM2Bins = 100;
+  double dM2BinSize = (nM2Max - nM2Min)/nM2Bins;
+
+  TH1D* hgammaEnergy = new TH1D("Energy" ,                    // plot label
+                                "Gamma Energy Distribution",  // title
+                                nEnergyBins,                  // x number of bins
+                                nEnergyMin,                   // x lower bound
+                                nEnergyMax);                  // x upper bound
+
+  TH1D* hgammaTheta = new TH1D("#theta" ,                      // plot label
+                               "Gamma #theta Distribution",    // title
+                               nThetaBins,                    // x number of bins
+                               nThetaMin,                     // x lower bound
+                               nThetaMax);                    // x upper bound
+
+  TH1D* hm2 = new TH1D("M_{A'}^{2}" ,             // plot label
+                       "Missing Mass Squared",    // title
+                       nM2Bins,                   // x number of bins
+                       nM2Min,                    // x lower bound
+                       nM2Max);                   // x upper bound
+
+  TH2F* henergytheta = new TH2F("scatter",            // plot label
+                                "#theta vs Energy",   // title
+                                nThetaBins,           // x num of bins
+                                nThetaMin,            // x min
+                                nThetaMax,            // x max
+                                nEnergyBins,          // y num of bins
+                                nEnergyMin,           // y min
+                                nEnergyMax);          // y max
+
+  // go through all entries and fill the histograms
+
+  for (Int_t i=0;i<neplus;i++) {
+    Hits_Info->GetEntry(i);
+
+    if (pID == 22) { // gammas only
+      hgammaEnergy->Fill(energy,scaleFactor / dEnergyBinSize);
+      hgammaTheta->Fill(theta*pi*1000/180, scaleFactor/dThetaBinSize);
+      henergytheta->Fill(theta*pi*1000/180, energy);
+      hm2->Fill(2*me*(5000 - energy*(1 + (5000/(2*me))*pow(theta*pi/180,2))), scaleFactor / dM2BinSize);
     }
+  }
 
-  TCanvas* can1 = new TCanvas("can1", "Energy_and_Theta", 1000, 500);
-  TCanvas* can2 = new TCanvas("can2", "Mass", 1000, 500);
-  can1->Divide(2,2); 
-  can1->cd(1); engTheta->DrawCopy();
-  can1->cd(3); energyH->DrawCopy();
-  can1->cd(4); thetaH->DrawCopy();
+  // create canvas and draw histogram
+  TCanvas* canvas = new TCanvas("canvas", "canvas", 700, 700);
+  //canvas->SetCanvasSize(2000, 1000);
+  canvas->Divide(3,2);
+  TPad* p;
 
-  can2->cd(); M2->DrawCopy();
+  p = (TPad*)canvas->cd(2);
+  p->SetLogy();
+  p->SetGrid();
+  hgammaEnergy->SetFillColor(kBlue);
+  hgammaEnergy->SetFillStyle(3001);
+  hgammaEnergy->Draw();
+  hgammaEnergy->GetXaxis()->SetTitle("Energy (MeV)");
+  hgammaEnergy->GetYaxis()->SetTitle("Photons per MeV per Second (MeV^{-1} s^{-1})");
+  hgammaEnergy->GetXaxis()->CenterTitle();
+  hgammaEnergy->GetYaxis()->CenterTitle();
 
-  
+  p = (TPad*)canvas->cd(5);
+  p->SetLogy();
+  p->SetGrid();
+  hgammaTheta->SetFillColor(kBlue);
+  hgammaTheta->SetFillStyle(3001);
+  hgammaTheta->Draw();
+  hgammaTheta->GetXaxis()->SetTitle("#theta (mrad)");
+  hgammaTheta->GetXaxis()->CenterTitle();
+  hgammaTheta->GetYaxis()->SetTitle("Photons per mrad per Second (mrad^{-1} s^{-1})");
+  hgammaTheta->GetYaxis()->CenterTitle();
+
+  p = (TPad*)canvas->cd(1);
+  p->SetGrid();
+  hm2->SetFillColor(kBlue);
+  hm2->SetFillStyle(3001);
+  hm2->GetXaxis()->SetTitle("M_{A'}^{2} (MeV^{2})");
+  hm2->GetYaxis()->SetTitle("Photons per MeV^{2} per Second (MeV^{-2} s^{-1})");
+  hm2->GetXaxis()->CenterTitle();
+  hm2->GetYaxis()->CenterTitle();
+  hm2->Draw();
 
 
+  p = (TPad*)canvas->cd(4);
+  p->SetLogy();
+  p->SetGrid();
+  hm2->Draw();
+
+  p = (TPad*)canvas->cd(3);
+  // Hits_Info->Draw("theta:Energy", "PDGID==22 && theta>2 && theta<5 && Energy>10 && Energy<510");
+  henergytheta->Draw();
+  henergytheta->GetYaxis()->SetTitle("Energy (MeV)");
+  henergytheta->GetXaxis()->SetTitle("#theta (mrad)");
+  henergytheta->GetXaxis()->CenterTitle();
+  henergytheta->GetYaxis()->CenterTitle();
 
 }
