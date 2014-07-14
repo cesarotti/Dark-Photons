cba0cc06bfbf74cb39fbfdb33d56c0d831fe5ba2
Brian Shin <bys8@cornell.edu>
2014-07-14 16:10:27 -0400

    Added 4PlotsInelastic/ . Also added 4PlotsInelastic/standard-build/ to .gitignore
    Former-commit-id: 2fbae4962f42c69d898bf0e2b36a819f8d6c90df

---
+++ 4PlotsInelastic/standard/include/BasicHit.hh
@@ -0,0 +1,86 @@
+/*
+ *Dark Photon Calorimeter Hit
+ *
+ *!!!History 
+ *   CJC 6.17.14 created
+ *
+ */
+
+#ifndef BasicHit_h
+#define BasicHit_h 1
+
+#include "G4VHit.hh"
+#include "G4THitsCollection.hh"
+#include "G4Allocator.hh"
+#include "G4ThreeVector.hh"
+#include "tls.hh"
+
+//Calorimeter hit class
+
+class BasicHit : public G4VHit
+{
+public:
+  BasicHit();
+  BasicHit(const BasicHit&);
+  virtual ~BasicHit();
+
+  //operators
+  const BasicHit& operator =(const BasicHit&);
+  G4int operator==(const BasicHit&) const;
+
+  inline void* operator new(size_t);
+  inline void operator delete(void*);
+
+  //methods from base class
+  virtual void Draw();
+  virtual void Print();
+
+  //Set methods
+
+  void SetTotalEnergy(G4double energy)
+  {fTotalEnergy = energy;};
+  void SetPosition(G4ThreeVector position)
+  {fPos = position; };
+  void SetPDGID(G4int id)
+  {fPDGID = id; };
+
+
+  //Get methods
+
+  G4double GetTotalEnergy()
+  {return fTotalEnergy;};
+  G4ThreeVector GetPosition()
+  {return fPos;};
+  G4int GetPDGID()
+  {return fPDGID;};
+
+
+private:
+
+  G4double fTotalEnergy; //total energy of particle
+  G4ThreeVector fPos;  //position of particle
+  G4int fPDGID; //particle ID
+
+};
+
+//Allow for a hits collection of BasicHits
+typedef G4THitsCollection<BasicHit> BasicHitsCollection; 
+
+extern G4ThreadLocal G4Allocator<BasicHit>* BasicHitAllocator;
+
+
+//Easy source of Segmentation Faults
+inline void* BasicHit::operator new(size_t)
+{
+  if(!BasicHitAllocator)
+    BasicHitAllocator = new G4Allocator<BasicHit>; 
+  return (void *) BasicHitAllocator->MallocSingle();
+}
+
+inline void BasicHit::operator delete(void *hit)
+{
+  BasicHitAllocator->FreeSingle((BasicHit*) hit);
+}
+
+#endif
+
