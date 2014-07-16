3c9c9443b5e551acf53844dfc41add21ff623af7
Brian Shin <bys8@cornell.edu>
2014-07-16 12:31:04 -0400

    modified 4PlotsInelastic
    Former-commit-id: bdae6985d0682d12ed7dc659d21154736458ccaf

--- 4PlotsInelastic/standard/src/PositronPhysicsList1.cc
+++ 4PlotsInelastic/standard/src/PositronPhysicsList1.cc
@@ -30,12 +30,31 @@
 
 #include "G4eplusAnnihilation.hh"
 
+// ####################################### BRIAN SHIN ####################################
+#include "G4PionZero.hh"
+#include "G4Decay.hh"
+#include "G4DecayTable.hh"
+#include "G4PhaseSpaceDecayChannel.hh"
+#include "G4DalitzDecayChannel.hh"
+
+#include "G4PositronNuclearProcess.hh"
+#include "G4ElectronNuclearProcess.hh"
+#include "G4ElectroVDNuclearModel.hh"
+
+#include "G4BosonConstructor.hh"
+#include "G4LeptonConstructor.hh"
+#include "G4MesonConstructor.hh"
+#include "G4BaryonConstructor.hh"
+#include "G4IonConstructor.hh"
+#include "G4ShortLivedConstructor.hh"
+// ####################################### BRIAN SHIN ####################################
+
 #include "G4VEmProcess.hh"
 
 
 PositronPhysicsList1::PositronPhysicsList1()
 {
-  defaultCutValue = 1.0*mm; //As me if you need energy cut offs, too
+  defaultCutValue = 1.0*mm;
   SetVerboseLevel(0);
 }
 
@@ -44,20 +63,42 @@ PositronPhysicsList1::~PositronPhysicsList1()
 
 void PositronPhysicsList1::ConstructParticle()
 {
-  G4Gamma::GammaDefinition();
-  G4Positron::PositronDefinition();
-  G4Electron::ElectronDefinition();
-  G4Proton::ProtonDefinition();
+  //G4Gamma::GammaDefinition();
+  //G4Positron::PositronDefinition();
+  //G4Electron::ElectronDefinition();
+  //G4Proton::ProtonDefinition();
+
+  G4BosonConstructor bosonCons;
+  bosonCons.ConstructParticle();
+
+  G4LeptonConstructor lepCons;
+  lepCons.ConstructParticle();
+
+  G4MesonConstructor mesonCons;
+  mesonCons.ConstructParticle();
+
+  G4IonConstructor ionCons;
+  ionCons.ConstructParticle();
+
+  G4BaryonConstructor baryonCons;
+  baryonCons.ConstructParticle();
+
+  G4ShortLivedConstructor slCons;
+  slCons.ConstructParticle();
 }
 
 void PositronPhysicsList1::ConstructProcess()
 {
   AddTransportation();
-  ConstructEM();
+  //Construct2Gamma();
+  //ConstructBremsstrahlung();
+  ConstructPositronNuclear();
+  //ConstructElectronNuclear();
+  ConstructDecay();
 
 }
 
-void PositronPhysicsList1::ConstructEM()
+void PositronPhysicsList1::Construct2Gamma()
 {
   G4ParticleDefinition* positron = G4Positron::PositronDefinition();
   G4ProcessManager* pman = positron->GetProcessManager();
@@ -65,13 +106,92 @@ void PositronPhysicsList1::ConstructEM()
 
 
   G4VEmProcess* eplusProc = new G4eplusAnnihilation();
-  //How to bias
-  eplusProc->SetCrossSectionBiasingFactor(1e+06, true); 
+  eplusProc->SetCrossSectionBiasingFactor(1e+06, true);
   pman->AddProcess(eplusProc, 0, -1, 4);
- 
   
+
+  //pman->AddProcess(new G4eplusAnnihilation(), 0, -1, 4);
+ 
+
 }
 
+// ####################################### BRIAN SHIN ####################################
+void PositronPhysicsList1::ConstructBremsstrahlung() {
+  G4ParticleDefinition* electron = G4Electron::ElectronDefinition();
+  G4ProcessManager* pman = electron->GetProcessManager();
+  
+
+
+  G4eBremsstrahlung* brems = new G4eBremsstrahlung();
+  brems->SetCrossSectionBiasingFactor(100, true);
+  pman->AddProcess(brems, -1, 3, 3);
+}
+
+void PositronPhysicsList1::ConstructPositronNuclear()
+{
+  // get pointer to positron process manager
+  G4ParticleDefinition* positron = G4Positron::PositronDefinition();
+  G4ProcessManager* pman = positron->GetProcessManager();
+
+  // get pointer to positron-nuclear process
+  G4PositronNuclearProcess* pnproc = new G4PositronNuclearProcess("PositronNuclear");
+  
+  // register positron-nuclear model
+  G4ElectroVDNuclearModel* pnmodel = new G4ElectroVDNuclearModel();
+  pnproc->RegisterMe(pnmodel);
+
+  // set biasing
+  pnproc->BiasCrossSectionByFactor(10000.);
+
+  // add process to process manager
+  pman->AddDiscreteProcess(pnproc);
+}
+
+void PositronPhysicsList1::ConstructElectronNuclear()
+{
+  // get pointer to positron process manager
+  G4ParticleDefinition* electron = G4Electron::ElectronDefinition();
+  G4ProcessManager* pman = electron->GetProcessManager();
+
+  // get pointer to positron-nuclear process
+  G4ElectronNuclearProcess* pnproc = new G4ElectronNuclearProcess("ElectroNuclear");
+  
+  // register positron-nuclear model
+  G4ElectroVDNuclearModel* pnmodel = new G4ElectroVDNuclearModel();
+  pnproc->RegisterMe(pnmodel);
+
+  // set biasing
+  pnproc->BiasCrossSectionByFactor(10000.);
+
+  // add process to process manager
+  pman->AddDiscreteProcess(pnproc);
+}
+
+void PositronPhysicsList1::ConstructDecay() {
+  G4PionZero* pionZero = G4PionZero::PionZeroDefinition();
+  G4ProcessManager* pman = pionZero->GetProcessManager();
+
+  G4Decay* decayProc = new G4Decay();
+
+  pman->AddProcess(decayProc);
+  pman->SetProcessOrdering(decayProc, idxPostStep);
+  pman->SetProcessOrdering(decayProc, idxAtRest);
+
+  G4DecayTable* table = pionZero->GetDecayTable();
+
+  // create a decay channel
+  G4VDecayChannel* mode;
+
+  // pi0 -> gamma + gamma
+  mode = new G4PhaseSpaceDecayChannel("pi0",0.988,2,"gamma","gamma");
+  table->Insert(mode);
+
+  // pi0 -> gamma + e+ + e-
+  mode = new G4DalitzDecayChannel("pi0",0.012,"e-","e+");
+  table->Insert(mode);
+}
+
+// ###################################### BRIAN SHIN ####################################
 
 void PositronPhysicsList1::SetCuts()
 {
@@ -80,7 +200,3 @@ void PositronPhysicsList1::SetCuts()
   SetCutValue(defaultCutValue, "e+");
   SetCutValue(defaultCutValue, "proton");
 }
-
-
-
-
