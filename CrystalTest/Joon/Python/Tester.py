d1bc08b061e742e9b5e18c8901144ae329f6fad5
Joonpark13 <Joon@erdhcp-10-33-80-74.eduroam.cornell.edu>
2014-07-20 22:27:19 -0400

    Ready for Crystal Testing
    Former-commit-id: 1f33d5527c7ac9b1a5efe82f9e0cd152bca23915

--- CrystalTest/Joon/Python/Tester.py
+++ CrystalTest/Joon/Python/Tester.py
@@ -1,4 +1,31 @@
+import threading
+import serial
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+class SerialReader(threading.Thread):
+    # Defines a thread for writing serial data to text file
+    def __init__(self, port, chunkSize=1024, loops = 1):
+        threading.Thread.__init__(self)
+        self.chunkSize = chunkSize  # size of a single chunk (items, not bytes)
+        self.port = port            # serial port handle
+        self.loops = loops
+    
+    def run(self):
+        port = self.port
+        
+        txtfl = open("voltagedata.txt", 'w')
+      
+        for i in range(self.loops):
+            data = port.read(self.chunkSize*2)
+            data = np.fromstring(data, dtype=np.uint16)
+            np.set_printoptions(threshold = 'nan')
+            txtfl.write(" " + str([-10 for x in range(10)])+ " ")
+            txtfl.write(str(data))
+        
+        txtfl.close()
+
 
 class Plotter(object):
     #datastring: array of strings containing voltage data
@@ -14,21 +41,6 @@ class Plotter(object):
     #filters the data
     def filterdata(self):
         for i in range(len(self.datastring)):
-            """
-            if self.datastring[i] == "[":
-                pass
-            elif "[" in self.datastring[i] and "]" in self.datastring[i]:
-                tempstring = self.datastring[i][:len(self.datastring[i]) - 2]
-                self.dta.append(float(tempstring))
-            elif "[" in self.datastring[i]:
-                tempstring = self.datastring[i][1:]
-                self.dta.append(float(tempstring))
-            elif "]" in self.datastring[i]:
-                tempstring = self.datastring[i][:len(self.datastring[i]) - 1]
-                self.dta.append(float(tempstring))
-            else:
-                self.dta.append(float(self.datastring[i]))
-            """
             temp = self.datastring[i].translate(None, '[],')
             if temp == "":
                 pass
@@ -68,40 +80,48 @@ class Plotter(object):
             else:
                 i += 1
         #throws out pulses cut off by the thread switching
-        l = 0
-        while l < range(len(self.listofdata)):
-            for m in range(self.rise,len(self.listofdata[l]) - self.tail):
-                if self.listofdata[l][m] == -10:
-                    self.listofdata.pop(l)
+        newlistofdata = []
+        for item in self.listofdata:
+            found = False
+            for num in item:
+                if num == -10:
+                    found = True
                     break
-                    
+            if found == False:
+                newlistofdata.append(item)
+            else:
+                found = False
+        self.listofdata = newlistofdata
+        
     
     #Simple plot
     def plotsimple(self, start = -1, end = -1):
         if start == -1 and end == -1:
             plt.plot(self.dta)
             plt.savefig(self.filedir + "//simpleplot.png", dpi = 500)
+            plt.show()
             plt.close()
         else:
             plt.plot(self.dta[start:end])
             plt.savefig(self.filedir + "//simpleplot%s" % (start) + "to" + str(end) + ".png", dpi = 500)
+            plt.show()
             plt.close()
     
     #Plots all pulses together on one graph,
     #then saves it as plot.png
-    def plotdatatog(self):
+    def plottog(self):
         for item in self.listofdata:
             plt.plot(item)
-        plt.savefig(self.filedir + "//plot.png", dpi = 500)
+        plt.savefig(self.filedir + "//plot.png", dpi = 750)
         plt.close()
 
     #Plots all pulses separately on individual graphs,
     #then saves them as plotn.png, where n is the plot number
-    def plotdatasep(self):
+    def plotsep(self):
         for i in range(0,len(self.listofdata)):
             plt.plot(self.listofdata[i])
             savename = "//plot%s.png" % (i + 1)
-            plt.savefig(self.filedir + savename, dpi = 500)
+            plt.savefig(self.filedir + savename)
             plt.close()
             
     #Saves pulses to textfile
@@ -110,15 +130,34 @@ class Plotter(object):
         txtfl.write(str(self.listofdata))
         txtfl.close()
 
-def main():
-    voltagedata = open("voltagedataTEMP.txt", 'r')
+
+def analyze(txtflname, trigger, rise, tail, filedir):
+    voltagedata = open(txtflname, 'r')
     voltages = voltagedata.read().split()
-    pltr = Plotter(voltages, 20, 5, 5, filedir = "//Users//Joon//OneDrive//Cornell//LEPPSummer2014DarkPhoton//Plots")
+    pltr = Plotter(voltages, trigger, rise, tail, filedir)
     pltr.filterdata()
+    #pltr.plotsimple()
     pltr.splicedata()
-    pltr.plotdatatog()
-    pltr.plotdatasep()
+    pltr.plottog()
     voltagedata.close()
 
+def main():
+    #PARAMETERS
+    serialportname = '/dev/tty.usbmodem1421'
+    txtflname = "voltagedata.txt"
+    trigger = 20
+    rise = 5
+    tail = 5
+    filedir = "//Users//Joon//OneDrive//Cornell//DarkPhoton//Plots//July20"
+    """
+    # Get handle to serial port
+    s = serial.Serial(serialportname)
+    # Create thread to read serial data.
+    thread = SerialReader(s, 2**13, 10)
+    thread.start()
+    thread.join()
+    """
+    analyze(txtflname, trigger, rise, tail, filedir)
+    
 if __name__ == '__main__':
     main()
