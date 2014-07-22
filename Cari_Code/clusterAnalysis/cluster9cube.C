ce308c035147c5971de6db5baaaf5c86ab616cbe
Cari Cesarotti <cjc359@cornell.edu>
2014-07-22 03:00:33 -0400

    more cluster analysis fixes

--- Cari_Code/clusterAnalysis/cluster9cube.C
+++ Cari_Code/clusterAnalysis/cluster9cube.C
@@ -25,11 +25,6 @@ int IDtoY(int id)
   return id/35-17;
 }
 
-int distance(int id1, int id2)
-{
-  return Max(Abs(IDtoX(id1)-IDtoX(id2)), Abs(IDtoY(id1)-IDtoY(id2)));
-}
-
 void cluster9cube() {
 
   TFile* file = new TFile("complete.root");
@@ -38,34 +33,32 @@ void cluster9cube() {
 
   int nEvents = tree->GetEntries(); 
 
-  double addresses[1225] = {};
+ double addresses[1225] = {};
   for (int k=0; k<1225; k++){
-    std::stringstream ss2; 
+    std::stringstream ss2;
     ss2 << k; 
-    string str = "Crystal_" + ss2.str();
-    const char* charstr = str.c_str();
+    string str = "Crystal_"+ss2.str();
+    const char* charstr = str.c_str(); 
     tree->SetBranchAddress(charstr, &addresses[k]);
   }
 
- int hit(-1);
-
- TTree* tree2 = (TTree *)file->Get("Signal2");
-
- tree2->SetBranchAddress("Hits", &hit);
-
- const int nEvents2 = tree2->GetEntries();
-
  TH2D* gammaH = new TH2D("gammaH", "Photon_Pos_&_Energy", 35,-17,17,35,-17,17);
 
- TH1D* sumEnergyH = new TH1D("effectH", "Energy_Entirety_per_hit", 500, 0, 1);
+ double energyThresHi = 50.;
+ double energyThresLow = .5;
 
  int hitMap[1225] = {};
 
- for (int k=0; k<nEvents2; k++)
+ for (int w=0; w<nEvents; w++)
    {
-     tree2->GetEntry(k); 
-     if (hit>0) {hitMap[hit]+=1;} //shows pile up between events too
-   }
+     tree->GetEntry(w); 
+     for (int z=0; z<1225; z++)  
+       { 
+       if (addresses[z]>energyThresHi)
+       { cout << z << endl;
+	 hitMap[z]+=1;} //shows pile up between events too
+       }
+ } 
 
    for (int i=0; i<nEvents; i++)
      {
@@ -75,27 +68,31 @@ void cluster9cube() {
        for (int j=0; j<1225; j++) 
 	 {
 	   if (hitMap[j] > 0)
-	     { int x = IDtoX(j);
+	     { cout << "here1" << endl;
+	       int x = IDtoX(j);
 	       int y = IDtoY(j);
 	       if (addresses[j] >0)
 		 {
 		   //breadth first search
+		   double xPos(0.), yPos(0.), energy(0.);
 		   for (int q=-1; q<2; q++)
 		     {for (int f=-1; f<2; f++)
-			 {energySpread[(f+1)+3*(q+1)]=
+			 { cout << "here" << endl;
+			   energySpread[(f+1)+3*(q+1)]=
 			     addresses[CoordtoID(x+f, y+q)];
 			   gammaH->
 			     Fill(x+f,y+q, addresses[CoordtoID(x+f,y+q)]);
 			   crysID[(f+1)+3*(q+1)] =
 						 CoordtoID(x+f,y+q);
+			   xPos+=addresses[CoordtoID(x+f, y+q)]*(x+f); 
+			   yPos+=addresses[CoordtoID(x+f, y+q)]*(y+q); 
+			   energy+=addresses[CoordtoID(x+f, y+q)]; 
 			 }
 
 		     }
-		   double energyTot(0.);
-		   int a=0;
-		   while(a<9) {energyTot+=energySpread[a]; a++;};
-		   sumEnergyH->Fill((energyTot/350.));
-
+		   cout << "Photon Energy: " << energy << endl;
+		   cout << "Photon Position: (" << xPos*5.0/energy << 
+		     ", " << yPos*5.0/energy << ")" << endl; 
 		 }
 	     }
 	 }
@@ -104,10 +101,9 @@ void cluster9cube() {
 TCanvas* canvas = new TCanvas("canvas", "canvas", 700,700);
  canvas->Divide(2,1);
 
-canvas->cd(1);
+canvas->cd();
 gammaH->Draw("BOX");
- canvas->cd(2); 
- sumEnergyH->Draw();
+
 
 
 }
